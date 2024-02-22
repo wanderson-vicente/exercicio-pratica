882421e67ca49d5840d45230d21ec0d0f895cfed
wanderson-vicente <wandersonvicente@gmail.com>
2024-02-22 06:37:25 -0300

    feat - Atualização bmi.js

--- imc/bmi.js
+++ imc/bmi.js
@@ -6,8 +6,8 @@ const { imcCalc } = require('./utils/imcCalc');
 function main() {
   const weightInKg = readline.questionFloat('Digite seu peso em quilos: ');
   const heightInCm = readline.questionInt('Digite sua altura em centimetros: ');
-  const imc = imcCalc(weightInKg, heightInCm);
-  console.log(`Seu IMC é de: ${imc.imc.toFixed(2)} , sua situação é ${imc.status} `);
+  const { imc, status } = imcCalc(weightInKg, heightInCm);
+  console.log(`Seu IMC é de: ${imc.toFixed(2)} , sua situação é ${status} `);
 }
 
 main();
