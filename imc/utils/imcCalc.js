e2cde5ece1affdccbd6a9893c51c3c69d7427147
wanderson-vicente <wandersonvicente@gmail.com>
2024-02-21 22:15:31 -0300

    feat - Exercicios 5 refatorado

--- imc/utils/imcCalc.js
+++ imc/utils/imcCalc.js
@@ -1,20 +1,18 @@
 function imcCalc(weight, height) {
-  const heightInMt = (height / 100);
+  const heightInMt = height / 100;
   const imc = weight / (heightInMt ** 2);
-  let status = '';
-  if (imc < 18.5) {
-      status = 'Abaixo do peso (magreza)';
-    } else if (imc >= 18.5 && imc < 24.9) {
-      status = 'Peso normal';
-    } else if (imc >= 25 && imc < 29.9) {
-      status = 'Acima do peso (sobrepeso)';
-    } else if (imc >= 30 && imc < 34.9) {
-      status = 'Obesidade grau I';
-    } else if (imc >= 35 && imc < 39.9) {
-      status = 'Peso normal';
-    } else {
-      status = 'Obesidade graus III e IV';
-    }
+
+  const imcStatuses = [
+    { min: 0, max: 18.5, status: 'Abaixo do peso (magreza)' },
+    { min: 18.5, max: 24.9, status: 'Peso normal' },
+    { min: 25, max: 29.9, status: 'Acima do peso (sobrepeso)' },
+    { min: 30, max: 34.9, status: 'Obesidade grau I' },
+    { min: 35, max: 39.9, status: 'Obesidade grau II' },
+    { min: 40, max: Infinity, status: 'Obesidade graus III e IV' },
+  ];
+
+  const { status } = imcStatuses.find(({ min, max }) => imc >= min && imc < max);
+
   return { imc, status };
 }
 
