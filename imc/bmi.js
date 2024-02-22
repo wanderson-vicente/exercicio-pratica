dbb8312a2cb9881f00dff2ce6d87a8118551ddaf
wanderson-vicente <wandersonvicente@gmail.com>
2024-02-21 21:32:26 -0300

    feat - Exercicios 3

--- imc/bmi.js
+++ imc/bmi.js
@@ -1,10 +1,13 @@
+const readline = require('readline-sync');
 const { imcCalc } = require('./utils/imcCalc');
 
-const weightInKg = 90;
-const heightInCm = 175;
+// const weightInKg = 90;
+// const heightInCm = 175;
 function main() {
+  const weightInKg = readline.question('Digite seu peso em quilos: ');
+  const heightInCm = readline.questionInt('Digite sua altura em centimetros: ');
   const imc = imcCalc(weightInKg, heightInCm);
-  console.log(imc);
+  console.log('Seu IMC é de: ' + imc.toFixed(2));
 }
 
 main();
