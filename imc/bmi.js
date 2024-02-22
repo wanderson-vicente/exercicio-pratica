45aa9bae90128a2bd544474b0be3f9918fd34222
wanderson-vicente <wandersonvicente@gmail.com>
2024-02-21 22:15:02 -0300

    feat - Exercicios 5 sem refatorar

--- imc/bmi.js
+++ imc/bmi.js
@@ -7,7 +7,7 @@ function main() {
   const weightInKg = readline.questionFloat('Digite seu peso em quilos: ');
   const heightInCm = readline.questionInt('Digite sua altura em centimetros: ');
   const imc = imcCalc(weightInKg, heightInCm);
-  console.log(`Seu IMC é de: ${imc.toFixed(2)}`);
+  console.log(`Seu IMC é de: ${imc.imc.toFixed(2)} , sua situação é ${imc.status} `);
 }
 
 main();
--- imc/utils/imcCalc.js
+++ imc/utils/imcCalc.js
@@ -1,7 +1,21 @@
 function imcCalc(weight, height) {
   const heightInMt = (height / 100);
   const imc = weight / (heightInMt ** 2);
-  return imc;
+  let status = '';
+  if (imc < 18.5) {
+      status = 'Abaixo do peso (magreza)';
+    } else if (imc >= 18.5 && imc < 24.9) {
+      status = 'Peso normal';
+    } else if (imc >= 25 && imc < 29.9) {
+      status = 'Acima do peso (sobrepeso)';
+    } else if (imc >= 30 && imc < 34.9) {
+      status = 'Obesidade grau I';
+    } else if (imc >= 35 && imc < 39.9) {
+      status = 'Peso normal';
+    } else {
+      status = 'Obesidade graus III e IV';
+    }
+  return { imc, status };
 }
 
 module.exports = { imcCalc };
